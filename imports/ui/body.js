a45874092c8c2ce4f703fefa6b42be1dbc2a3c14
Marjan Velkovski <marjan.velk@gmail.com>
2016-11-13 22:09:09 +0100

    FinishedTutorial

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,3 +1,4 @@
+import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 import  { ReactiveDict } from 'meteor/reactive-dict';
 
@@ -9,6 +10,7 @@ import './body.html';
 
 Template.body.onCreated(function bodyOnCreated() {
     this.state = new ReactiveDict();
+    Meteor.subscribe('tasks');
 });
 
 Template.body.helpers({
